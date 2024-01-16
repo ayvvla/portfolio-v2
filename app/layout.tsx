b47156f15b014725ea5560155f5bf41abdf7d7e9
Ayoola Michael <65915666+ayvvla@users.noreply.github.com>
2024-01-16 02:24:24 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Ayoola Michael | Frontend Web Developer',
+  icons:'favicon.ico',
   description: 'Ayoola Michael | Frontend Web Developer',
 }
 
